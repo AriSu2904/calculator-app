d53465c9262b879979cb9b8f4c5cd6f7bcc71d01
AriSu2904 <arisusanto290401@gmail.com>
2023-10-15 22:29:12 +0700

    Sum Function

---
+++ Main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	calculator_module "github.com/AriSu2904/calculator-module"
+)
+
+func main() {
+	fmt.Println(calculator_module.Sum(5, 5))
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/AriSu2904/calculator-app
 
 go 1.21.3
+
+require github.com/AriSu2904/calculator-module v1.0.0 // indirect
